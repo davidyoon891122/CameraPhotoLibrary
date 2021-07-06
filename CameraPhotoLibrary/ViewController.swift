872bd0080fb83cd2a52f303c601a4159f6fe2c56
davidyoon <qqpo12@naver.com>
2021-07-06 22:56:48 +0900

    add layout

--- CameraPhotoLibrary/ViewController.swift
+++ CameraPhotoLibrary/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func btnCaptureImageFromCamera(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnLoadImageFromLibrary(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func btnRecordVideoFromCamera(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnLoadVideoFromCamera(_ sender: UIButton) {
+    }
 }
 
